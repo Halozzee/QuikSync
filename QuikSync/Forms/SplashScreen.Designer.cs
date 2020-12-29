10a1c3c21597c002011a64d9e0dd4acdb2d252cd
Halozzee <aanton2001@gmail.com>
2020-12-29 19:53:40 +0500

    Splash screen added and it's log is now working.

---
+++ QuikSync/Forms/SplashScreen.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace QuikSync.Forms
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            this.progressPanel = new System.Windows.Forms.Panel();
+            this.textLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logoPictureBox
+            // 
+            this.logoPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.logoPictureBox.Image = global::QuikSync.Properties.Resources.LogoNotext;
+            this.logoPictureBox.Location = new System.Drawing.Point(90, 38);
+            this.logoPictureBox.Name = "logoPictureBox";
+            this.logoPictureBox.Size = new System.Drawing.Size(176, 174);
+            this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logoPictureBox.TabIndex = 13;
+            this.logoPictureBox.TabStop = false;
+            // 
+            // progressPanel
+            // 
+            this.progressPanel.BackColor = System.Drawing.Color.Aqua;
+            this.progressPanel.Location = new System.Drawing.Point(0, 259);
+            this.progressPanel.Name = "progressPanel";
+            this.progressPanel.Size = new System.Drawing.Size(10, 33);
+            this.progressPanel.TabIndex = 14;
+            // 
+            // textLabel
+            // 
+            this.textLabel.AutoSize = true;
+            this.textLabel.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textLabel.Location = new System.Drawing.Point(12, 241);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(0, 13);
+            this.textLabel.TabIndex = 15;
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(355, 263);
+            this.Controls.Add(this.textLabel);
+            this.Controls.Add(this.progressPanel);
+            this.Controls.Add(this.logoPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SplashScreen";
+            this.Load += new System.EventHandler(this.SplashScreen_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.SplashScreen_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox logoPictureBox;
+        private System.Windows.Forms.Panel progressPanel;
+        private System.Windows.Forms.Label textLabel;
+    }
+}
---
+++ QuikSync/Forms/SplashScreen.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinFormAnimation;
+
+namespace QuikSync.Forms
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+
+            MaxValue = 100;
+        }
+
+        private void SplashScreen_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void SplashScreen_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(new Pen(Color.FromArgb(38, 38, 42), 2), this.DisplayRectangle);
+        }
+
+        public void IncrementValue(int inc = 1)
+        {
+            CurrentValue += inc;
+            progressPanel.Width = (int)((CurrentValue / (double)MaxValue * 100) * (this.Width/(double)100));
+            this.Update();
+        }
+
+        public void SetText(string text) 
+        {
+            textLabel.Text = text;
+            this.Update();
+        }
+
+        public int MaxValue {get; set;}
+        public int CurrentValue {get; set;}
+    }
+}
